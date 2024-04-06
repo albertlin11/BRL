4e162fad91e3e7200b540a33eeea825b7ac9a750
albertlin11 <52873299+albertlin11@users.noreply.github.com>
2024-04-06 12:15:05 +0800

    Add files via upload

---
+++ Bayesian_S,Apair_new/Bayesian_S,Apair_new/model_TCAD=0_Explo=1_S,Apair/main.py
@@ -0,0 +1,299 @@
+from prior_prob import prior_prob
+import numpy as np
+from env import env
+import os
+import random
+import random as python_random
+import csv
+import pandas as pd
+from find_Rs import find_Rs
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import matplotlib.pyplot as plt
+
+os.makedirs("data_old_reward_prior=0_2", exist_ok = 1)
+
+tf.random.set_seed(1)
+os.environ['PYTHONHASHSEED'] = '0'
+random.seed(1)
+python_random.seed(1)
+np.random.seed(1)
+
+def decoder(action):
+    binary = format(action, '010b')
+    choice = binary[:3]
+
+    ion = int(binary[3]) + 1
+    dose = 2 * int(choice[0]) + int(binary[4]) + 1
+    ion_energy = 2 * int(choice[1]) + int(binary[5]) + 1
+    wavelength = int(binary[6]) + 1
+    rep_rate = int(binary[7]) + 1
+    power = 2 * int(choice[2]) + int(binary[8]) + 1
+    temp = int(binary[9]) + 1
+    new_state = np.array([ion, dose, ion_energy, wavelength, 
+                            rep_rate, power, temp])
+    return new_state
+
+def change_TCAD(TCAD,reward,action,list):
+    if action not in list:
+        mul = abs(reward / TCAD[action])
+        TCAD[action] = reward
+        return TCAD, mul
+    else:
+        mul = 1
+        return TCAD, mul
+
+def save_prior_change_index(state,list):
+    change_list = []
+    i = 0
+    while(i<1024):
+        if i in list:
+            i += 1
+        else:
+            test = np.abs(decoder(i)-state)
+            if np.sum(test) == 1:
+                change_list.append(i)
+            i += 1
+    return change_list
+
+def prior_change(change_list,list,mul,TCAD):
+    i = 0
+    while(i < len(change_list)):
+        if change_list[i] in list:
+            i += 1
+        else:
+            TCAD[change_list[i]] = round(TCAD[change_list[i]] * mul,2)
+            i += 1
+    return TCAD
+
+def state_to_index(state):
+    binary = [0,0,0,0,0,0,0,0,0,0]
+    binary[3] = str(state[0] // 2)
+    binary[0] = str((state[1] - 1) // 2)
+    binary[4] = str((state[1] - 2 * int(binary[0])) // 2)
+    binary[1] = str((state[2] - 1) // 2)
+    binary[5] = str((state[2] - 2 * int(binary[1])) // 2)
+    binary[6] = str(state[3] // 2)
+    binary[7] = str(state[4] // 2)
+    binary[2] = str((state[5] - 1) // 2)
+    binary[8] = str((state[5] - 2 * int(binary[2])) // 2)
+    binary[9] = str(state[6] // 2)
+    out = ""
+    for i in range(10):
+        out = out + binary[i]
+    return int(out,2)+1
+
+class MakeModel:
+    def __init__(self,network = 0, batch_size=2,filepath="Save_Model.h5"):
+        self.network = network
+        self.model = self.create_model()
+        self.batch_size = batch_size
+        self.filepath = filepath
+
+    def create_model(self):
+        if self.network == 2:
+            input_layer = Input(shape = (2,), name = 'input_layer')
+            x = Dense(100,activation="tanh")(input_layer)
+            x = Dense(100,activation="tanh")(x)
+            x = Dense(28,activation="tanh")(x)
+            output_layer = Dense(1, name = "output_layer")(x)
+            model = Model(inputs = input_layer, outputs = output_layer)
+            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),loss='mse')
+            return model
+        else:
+            input_layer = Input(shape = (2,), name = 'input_layer')
+            x = Dense(100,activation="tanh")(input_layer)
+            x = Dense(100,activation="tanh")(x)
+            x = Dense(100,activation="tanh")(x)
+            x = Dense(100,activation="tanh")(x)
+            x = Dense(100,activation="tanh")(x)
+            x = Dense(28,activation="tanh")(x)
+            output_layer = Dense(1, name = "output_layer")(x)
+            model = Model(inputs = input_layer, outputs = output_layer)
+            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),loss='mse')
+            return model
+    
+    def predict(self, state):
+        return self.model.predict(state)
+
+    def train(self, state, target):
+        callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=self.filepath,
+        save_weights_only=True,
+        monitor='loss',
+        save_best_only=True)
+        self.model.fit(state, target, epochs=1000, batch_size=self.batch_size, 
+        callbacks=[callback], verbose=0)
+    
+    def load_model(self):
+        self.model.load_weights(self.filepath)
+
+def generate_all_input(state):
+    total = []
+    state = np.array(state)
+    for i in range(1,1025):
+        new = np.append(state,i)
+        total.append(list(new))
+    total = np.reshape(total,(-1,2))
+    return total
+
+
+a = pd.read_csv("RL_Input_parameter.csv")
+a = a.values
+
+for k in range(3,6):
+
+    tf.random.set_seed(1)
+    os.environ['PYTHONHASHSEED'] = '0'
+    random.seed(1)
+    python_random.seed(1)
+    np.random.seed(1)
+
+    network_num = a[k][0]
+    batch_size_num = a[k][1]
+    radius = a[k][2]
+    
+    filepath2 = "./data_old_reward_prior=0_2/RL_network="+str(network_num)+ "_batch_size="+\
+        str(batch_size_num)+".csv"
+
+    with open(filepath2, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Ion', 'Dose', 'Ion Energy', 'Wavelength',
+                            'Rep. Rate', 'Power', 'Temperature', 'Rs'])
+    
+    
+    input_list = np.array([[1,1],[1025,1025]])
+    input_list = np.log10(input_list)
+    in_standard = MinMaxScaler()
+    input_list = in_standard.fit_transform(input_list)
+
+
+    out_standard = MinMaxScaler()
+    out_list = np.array([0,-3]).reshape(-1,1)
+    out_list = out_standard.fit_transform(out_list)
+
+
+    environment = env(reward_func=0)
+    prior = prior_prob(reward=0)
+    prior = np.array(prior)
+
+    model1 = MakeModel(network=int(network_num),batch_size=int(batch_size_num))
+    
+
+    save_state = []
+    save_reward = []
+    
+    save = []
+    best = 1e11
+    besttimestep = 0
+    epsilon = 0.5
+    difference=[]
+
+    for ep in range(5):
+        state = environment.reset()
+        timestep = 0
+        done = False
+        while not done:
+            state_index = state_to_index(state)
+            timestep+=1
+            if ep != 0 and timestep == 1:
+                epsilon = round(epsilon * 0.9,2)
+
+
+            if np.random.randint(1,11)/10 <= epsilon:
+                action = np.random.randint(0,1024)
+            else:
+                pred_input = generate_all_input(state_index)
+                pred_input = np.log10(pred_input)
+                pred_input = in_standard.transform(pred_input)
+                pred = model1.predict(pred_input)
+                pred = out_standard.inverse_transform(pred)
+                pred = np.reshape(pred,(1,-1))[0]
+                pred = -(10 ** (-pred))
+                pred = pred
+                action = np.argmax(pred)
+
+
+            next_state,reward,done,env_timestep,Rs,true_label = environment.response(action)
+            print("timestep",(ep*10+timestep),"Rs",Rs)
+
+            true_param = np.append(true_label, Rs)
+            
+            with open(filepath2, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(true_param)
+            
+            next_state_index = state_to_index(next_state)
+
+            memory = [state_index,action+1,next_state_index,done]
+
+            if Rs >= 1000:
+                new_Rs = 1000
+            else:
+                new_Rs = Rs
+
+            training_in = []
+            training_out1 = []
+
+            if memory not in save_state:
+                save_state.append(memory)
+                save_reward.append(new_Rs)
+
+            for j in range(len(save_state)):
+                if save_state[j][3] == 1:
+                    training_in.append(save_state[j][0:2])
+                    save_q = -np.log10(save_reward[j])
+                    training_out1.append(save_q)
+                else:
+                    training_in.append(save_state[j][0:2])
+                    pred_input = generate_all_input(save_state[j][2])
+                    pred_input = np.log10(pred_input)
+                    pred_input = in_standard.transform(pred_input)
+                    pred1 = model1.predict(pred_input)
+                    pred1 = out_standard.inverse_transform(pred1)
+                    pred1 = np.reshape(pred1,(1,-1))[0]
+                    pred1 = -(10 ** (-pred1))
+                    pred1 = pred1
+                    action = np.argmax(pred1)
+                    save_q1 = -np.log10(save_reward[j]+0.2*np.abs(pred1[action]))
+                    if save_q1 <= -3:
+                        save_q1 = -3
+                    training_out1.append(save_q1)
+            
+            train_in = np.log10(training_in)
+
+            train_in = in_standard.transform(np.reshape(train_in,(-1,2)))
+            train_out1 = out_standard.transform(np.reshape(training_out1,(-1,1)))
+
+            if Rs < best:
+                best = Rs
+                besttimestep = (ep*10+timestep)
+                
+            if (ep*10+timestep) <= 49:
+                model1.train(train_in,train_out1)
+
+            model1.load_model()
+
+            state = next_state
+        
+            if action not in save:
+                save.append(action)
+    print("Best Timestep:", besttimestep, 
+              "Minimum Rs: ", best)
+
+
+    if filepath2 != 0:
+        with open(filepath2, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Best Timestep", besttimestep, 
+                                "Minimum Rs", best])
+        count, count_before_best = find_Rs(filepath2)
+        with open(filepath2, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Count berfore best timestep", 
+                                count_before_best, 
+                                "Total count", count])
+
---
+++ Bayesian_S,Apair_new/Bayesian_S,Apair_new/model_TCAD=1_Explo=1_S,Apair_prior=fixed/Fig.m
@@ -0,0 +1,50 @@
+clear;clf;close all;clc
+
+if not(isfolder("RL_fig_prior_fixed"))
+    mkdir RL_fig_prior_fixed
+end
+I = readtable("RL_Input_parameter.csv");
+for i = 1:height(I)
+    clf
+    network = string(I{i,1});
+    batch_size = string(I{i,2});
+    radius = string(I{i,3});
+
+
+    input = "RL_network=" + network + "_batch_size="+batch_size;
+    file = string(input);
+
+
+    filename = file + '_prior_fixed.csv';
+    str1 = './data_old_reward_prior_fixed_2/';
+    path = string(str1) + filename;
+
+    A = csvread(path,1,0,[1,0,50,7]);
+    A = log10(A);
+    y=-A(1:50,8);
+    plot(y,'LineWidth',2,'Marker','o');
+    xlabel('Timesteps','FontSize',30)
+    ylim([-11,-1.5])
+    xlim([1,50])
+    set(gca,'TickDir','out','FontSize',25, 'LineWidth', 2.0);
+    xlabel('Timesteps','FontSize',30)
+    if I{i,1} == 0
+        ylabel('- log_{10}(Reward)','FontSize',30)
+    else
+        ylabel('Reward','FontSize',30)
+    end
+    ylabel('- log_{10}(Reward)','FontSize',30)
+    %set(gcf, 'PaperPosition', [0 0 800 540]);
+    box off
+    ax2 = axes('Position',get(gca,'Position'),...
+               'XAxisLocation','top',...
+               'YAxisLocation','right',...
+               'Color','none',...
+               'XColor','k','YColor','k');
+    set(ax2,'YTick', []);
+    set(ax2,'XTick', []);
+    set(gca,'LineWidth',1.5);
+    save_filename = './RL_fig_prior_fixed/' + file + '.png';
+    saveas(gcf, save_filename)
+
+end
